4a854a99d56ab461861ee8e90fe0826a94af2cf7
Sartaj Mahmud <oishik1999@gmail.com>
2025-05-12 00:44:39 +0600

    Added the Git commands both(Terminal/cmd and GUI versions)

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,23 @@ public class Main {
         System.out.println("Hello World");
     }
 }
+
+/*
+Git commands from terminal/cmd
+
+1. git init  (Make sure you are in the right project directory)
+2. git add .  ( '.' represents all files)
+3. git commit -m "My First Commit Message"
+4. git branch -M master
+5. git remote add origin < URL >
+6. git push origin master
+
+
+Git steps from IDE -> GUI
+1. VCS -> Enable VCS
+2. VCS -> Git -> Remote -> Add the repository URL
+3. VCS -> Commit -> Select All the files -> Write a commit message -> press commit
+4. VCS -> Git -> Branches -> New Branch -> create a new branch with name
+5. VCS -> GIT -> Push (Make sure to check all the details of your commits)
+
+ */
