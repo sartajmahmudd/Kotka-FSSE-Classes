09164115c0764abd4379a4cacaa234c30a3f6119
Sartaj Mahmud <oishik1999@gmail.com>
2025-05-12 01:42:16 +0600

    Operators

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,30 @@ public class Main {
     public static void main(String[] args) {
 
         /*
-        a variable is a symbol (usually a letter) that represents a value or quantity that can change
+        Operators
 
-        Data types:
-        1. Integer -> 1, 2, 3, 4,
-        2. float -> 1.2334,4.25
-        3. double -> 1.24213526, 2.54256426
-        4. char -> a, 1, #,
-        5. String -> "Hello World", "asuhf`124"
+        Arithmetical
+        + , -, *, /, %
+
+        incre/decremental
+        ++, --
+
+        Logical
+        & , | , !
+
+        Comparison
+        <, <= , >, >=, ==, !=
 
          */
 
-        int variableOne = 50;
-        float variableTwo = 1.22f;
-        double variableThree = 1.24235235;
-        char variableFour = 'A';
-        String variableFive = "lsjfhkj12124@#%";
 
-        System.out.println(variableThree);
+        boolean value1 = (1 != 10) ;
+
+
+        boolean result = value1;
+
+
+        System.out.println(result);
 
     }
 }
