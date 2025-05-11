a2dfb7abfd34be14e73eb9e8f7c4e7ca640e1af7
Sartaj Mahmud <oishik1999@gmail.com>
2025-05-12 00:46:03 +0600

    removed the Git commands of GUI version

--- src/Main.java
+++ src/Main.java
@@ -15,11 +15,4 @@ Git commands from terminal/cmd
 6. git push origin master
 
 
-Git steps from IDE -> GUI
-1. VCS -> Enable VCS
-2. VCS -> Git -> Remote -> Add the repository URL
-3. VCS -> Commit -> Select All the files -> Write a commit message -> press commit
-4. VCS -> Git -> Branches -> New Branch -> create a new branch with name
-5. VCS -> GIT -> Push (Make sure to check all the details of your commits)
-
  */
